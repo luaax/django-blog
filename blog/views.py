7d36d718aa9ae13b1ae3a590949624fd52d499f1
Thalita Almeida <taetha874@gmail.com>
2023-06-21 11:29:29 -0300

    Atualiza django-blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def index (request):
+    return HttpResponse('Olá Django - index')
+
+def ola(request):
+    #return HttpResponse('Olá Django')
+    return render(request, 'index.html')
+
 # Create your views here.
