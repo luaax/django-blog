af57c4d9f5e632d0ee2b73dd75e4cf4ce092dc8b
luaax <taetha874@gmail.com>
2023-11-01 11:28:56 -0300

    atualização

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,10 @@ from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from django.contrib.auth.hashers import make_password # para criptografar a senha
 from django.contrib import messages
+
 from django.contrib.auth import get_user_model
 User = get_user_model() # obtém o model padrão para usuários do Django
+
 from accounts.forms import AccountSignupForm # importa o form de registro
 
 class AccountCreateView(CreateView):
@@ -13,9 +15,10 @@ class AccountCreateView(CreateView):
     form_class = AccountSignupForm # conecta o form a view
     success_url = reverse_lazy('login') # destino após a criação do novo usuário
     success_message = 'Usuário criado com sucesso!'
+
     def form_valid(self, form): # executa quando os dados estiverem válidos
-        form.instance.password = make_password(form.instance.password)
-        form.save()
-        messages.success(self.request, self.success_message)
-        return super(AccountCreateView, self).form_valid(form)
+     form.instance.password = make_password(form.instance.password)
+     form.save()
+     messages.success(self.request, self.success_message)
+     return super(AccountCreateView, self).form_valid(form)
 # Create your views here.
